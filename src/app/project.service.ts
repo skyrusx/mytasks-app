831118ad805d2c59fa9d909f7e3f9012804ee131
Ruslan Fedotov <progruson@gmail.com>
2021-01-22 04:50:01 +0700

    Добавлен коммент

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -24,6 +24,7 @@ export class ProjectService {
     return this.http.get<Project[]>(this.projectsUrl + '/projects');
   }
 
+  /** PUT: update the task on the server */
   updateTask(task: any): Observable<any> {
     const urlPart = [this.projectsUrl, 'projects', task.project_id, 'todos', task.id].join('/');
     const url = `${urlPart}?is_completed=${task.is_completed}`;
