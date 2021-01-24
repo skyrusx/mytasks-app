ade8173e20835ec23956283e49e14ca1bac2aac1
Ruslan Fedotov <progruson@gmail.com>
2021-01-24 23:27:45 +0700

    projectUrl -> baseUrl

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -9,7 +9,7 @@ import { Project } from './project';
 @Injectable({ providedIn: 'root' })
 export class ProjectService {
 
-  private projectsUrl = 'https://app-mytasks.herokuapp.com';
+  private baseUrl = 'https://app-mytasks.herokuapp.com';
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -21,12 +21,12 @@ export class ProjectService {
 
   /** GET projects from the server */
   getProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(this.projectsUrl + '/projects');
+    return this.http.get<Project[]>(this.baseUrl + '/projects');
   }
 
   /** PUT: update the task on the server */
   updateTask(task: any): Observable<any> {
-    const urlPart = [this.projectsUrl, 'projects', task.project_id, 'todos', task.id].join('/');
+    const urlPart = [this.baseUrl, 'projects', task.project_id, 'todos', task.id].join('/');
     const url = `${urlPart}?is_completed=${task.is_completed}`;
     return this.http.put(url, task, this.httpOptions);
   }
