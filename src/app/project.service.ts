98d186356331c61b4a2d1bb4eb614925d6f1cf84
Ruslan Fedotov <progruson@gmail.com>
2021-01-25 15:22:34 +0700

    Фикс формирования url

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -26,14 +26,14 @@ export class ProjectService {
 
   /** PUT: update the task on the server */
   updateTask(task: any): Observable<any> {
-    const urlPart = [this.baseUrl, 'projects', task.project_id, 'todos', task.id].join('/');
+    const urlPart = `${this.baseUrl}/projects/${task.project_id}/todos/${task.id}`;
     const url = `${urlPart}?is_completed=${task.is_completed}`;
     return this.http.put(url, task, this.httpOptions);
   }
 
   /** POST: add a new task to the server */
   addTask(task: any): Observable<any> {
-    const url = [this.baseUrl, 'todos'].join('/');
+    const url = `${this.baseUrl}/todos`;
     return this.http.post(url, task, this.httpOptions);
   }
 }
