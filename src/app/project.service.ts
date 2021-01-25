51c4c145dc0b73e3393f043114c946ff2e54eaa2
Ruslan Fedotov <progruson@gmail.com>
2021-01-26 03:49:57 +0700

    Добавлен метод для добавления Категории

--- src/app/project.service.ts
+++ src/app/project.service.ts
@@ -24,4 +24,9 @@ export class ProjectService {
   getProjects(): Observable<Project[]> {
     return this.http.get<Project[]>(this.baseUrl + '/projects');
   }
+
+  /** POST: add a new project to the server */
+  addProject(project: Project): Observable<Project> {
+    return this.http.post<Project>(this.baseUrl + '/projects', project, this.httpOptions);
+  }
 }
