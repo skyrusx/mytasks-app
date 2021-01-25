7fc621a549a7b8335aa0121ccbb4814d02ffc21c
Ruslan Fedotov <progruson@gmail.com>
2021-01-25 20:26:48 +0700

    Отключил автофокус в форме

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,6 +13,6 @@ export class HeaderComponent {
   constructor(public modal: MatDialog) {}
 
   openModal() {
-    this.modal.open(TaskComponent);
+    this.modal.open(TaskComponent, {autoFocus: false});
   }
 }
